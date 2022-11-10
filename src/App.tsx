9d3ef01dfd7f03600f9ce63e68e375ad92f192b3
supocompote <dz@>
2022-11-10 09:34:23 +0100

    Description de l'agent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import {Link, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import AgentPage from "./pages/AgentPage";
 
